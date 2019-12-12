c4989cf7f37d99694bf984287ecec68a9812bc15
jobala <japhethobalak@gmail.com>
2019-12-12 11:32:28 +0300

    Adds custom types

---
+++ common/types.h
@@ -0,0 +1,9 @@
+# pragma once
+
+typedef unsigned int   u32int;
+typedef          int   s32int;
+typedef unsigned short u16int;
+typedef          short s16int;
+typedef unsigned char  u8int;
+typedef          char  s8int;
+
